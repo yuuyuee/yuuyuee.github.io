6a5a3cba3b4dc98a95e909c651bdcaeadc888a32
Yue Yu <yuyue2200@hotmail.com>
2024-07-09 11:45:48 +0800

    update

--- docs/algorithm/binary_search_tree.c
+++ docs/algorithm/binary_search_tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 struct node {
     struct node* left;
@@ -32,21 +33,20 @@ struct node* node_alloc(int value) {
     return node;
 }
 
-// O(h)
 void node_free(struct node* node) {
     while (node) {
-        node_free(node->right);
-        struct node* ptr = node->left;
-        free(node);
-        node = ptr;
+        node_free(node->left);
+        struct node* ptr = node;
+        node = node->right;
+        free(ptr);
     }
 }
 
 void node_traverse(const struct node* node, void (*visitor)(int)) {
-    if (node) {
+    while (node) {
         node_traverse(node->left, visitor);
         visitor(node->value);
-        node_traverse(node->right, visitor);
+        node = node->right;
     }
 }
 
@@ -69,164 +69,177 @@ void node_traverse(const struct node* node, void (*visitor)(int)) {
 //                 node we found; and
 //              b) Go to this left child, i.e., current = current->left
 void node_traverse2(const struct node* node, void (*visitor)(int)) {
-  struct node* pre;
-  struct node* cur = (struct node*) node;
+    struct node* pre;
+    struct node* cur = (struct node*) node;
 
-  int i = 0;
-  while (cur) {
-    if (cur->left) {
-      // find the inorder predecessor of current
-      pre = cur->left;
-      while (pre->right && pre->right != cur)
-        pre = pre->right;
+    while (cur) {
+        if (cur->left) {
+            // find the inorder predecessor of current
+            pre = cur->left;
+            while (pre->right && pre->right != cur)
+                pre = pre->right;
 
-      // make sure as the right child of its inorder predecessor
-      if (pre->right) {
-        // revert the changes mode in the `if' part to restore the
-        // original tree i.e., fix the right child of predecessor
-        pre->right = NULL;
-        visitor(cur->value);
-        cur = cur->right;
-      } else {
-        pre->right = cur;
-        cur = cur->left;
-      }
-    } else {
-      visitor(cur->value);
-      cur = cur->right;
+            // make sure as the right child of its inorder predecessor
+            if (pre->right) {
+                // revert the changes mode in the `if' part to restore the
+                // original tree i.e., fix the right child of predecessor
+                pre->right = NULL;
+                visitor(cur->value);
+                cur = cur->right;
+            } else {
+                pre->right = cur;
+                cur = cur->left;
+            }
+        } else {
+            visitor(cur->value);
+            cur = cur->right;
+        }
     }
-  }
 }
 
-void stack_push(const void*** base, int* top, int* max, const void* value) {
-    if (*top >= *max) {
-        int new_max = *max == 0 ? 10 : *max * 2;
-        *base = realloc(*base, sizeof(**base) * new_max);
-        assert(*base && "realloc failed");
-        *max = new_max;
-    }
+struct stack {
+    const void** base;
+    int top;
+    int size;
+};
 
-    (*base)[((*top)++)] = value;
+void stack_init(struct stack* stack) {
+    stack->base = NULL;
+    stack->top = 0;
+    stack->size = 0;
 }
 
-const void* stack_pop(const void** base, int* top) {
-    assert(*top >= 0);
-    return base[(*top)--];
+void stack_free(struct stack* stack) {
+    if (stack->base)
+        free(stack->base);
+}
+
+void stack_extend(struct stack* stack, int size) {
+    assert(stack->size < size);
+    stack->base = stack->base
+            ? realloc(stack->base, size * sizeof(*stack->base))
+            : malloc(size * sizeof(*stack->base));
+    stack->size = size;
+}
+
+void stack_push(struct stack* stack, const void* value) {
+    if (stack->top == stack->size) {
+        int new_size = stack->size == 0 ? 64 : stack->size * 2;
+        stack_extend(stack, new_size);
+    }
+    stack->base[stack->top++] = value;
+}
+
+const void* stack_pop(struct stack* stack) {
+    assert(stack->top >= 0);
+    return stack->base[--stack->top];
+}
+
+int stack_empty(const struct stack* stack) {
+    return stack->top == 0;
 }
 
 void node_traverse3(const struct node* node, void (*visitor)(int)) {
-    const void** base;
-    int top = 0, max = 0;
+    struct stack stack;
 
-    while (node || top > 0) {
+    stack_init(&stack);
+    while (node || !stack_empty(&stack)) {
         while (node) {
-            stack_push(&base, &top, &max, node);
+            stack_push(&stack, node);
             node = node->left;
         }
-
-        node = (const struct node*) stack_pop(base, &top);
+        node = (const struct node*) stack_pop(&stack);
         visitor(node->value);
         node = node->right;
     }
+    stack_free(&stack);
 }
 
-struct binary_search_tree {
+// O(h)
+void node_insert(struct node** node, int value) {
+    while (*node)
+        node = (*node)->value > value ? &(*node)->left : &(*node)->right;
+    *node = node_alloc(value);
+}
+
+// O(h)
+int* node_search(struct node* node, int value) {
+    while (node && node->value != value)
+        node = node->value > value ? node->left : node->right;
+    return node ? &node->value : NULL;
+}
+
+struct bst {
     struct node* root;
     size_t size;
 };
 
-void bst_init(struct binary_search_tree* tree) {
+void bst_init(struct bst* tree) {
     tree->root = NULL;
     tree->size = 0;
 }
 
-
-void bst_free(struct binary_search_tree* tree) {
+void bst_free(struct bst* tree) {
     node_free(tree->root);
     tree->size = 0;
 }
 
-int bst_empty(const struct binary_search_tree* tree) {
-  return tree->size == 0;
+int bst_empty(const struct bst* tree) {
+    return tree->size == 0;
 }
 
 // Inorder traverse the binary tree with recursion
-void bst_traverse(const struct binary_search_tree* tree, void (*visitor)(int)) {
-    node_traverse(tree->root, visitor);
+void bst_traverse(const struct bst* tree, void (*visitor)(int)) {
+    node_traverse2(tree->root, visitor);
 }
 
-
-void NodeInsert(Node** node, int value) {
-  if (NodeEmpty(*node)) {
-    NodeInit(node, value);
-  } else if ((*node)->value > value) {
-    NodeInsert(&(*node)->left, value);
-  } else {
-    NodeInsert(&(*node)->right, value);
-  }
-
-  {
-    Node* p = *node;
-    while (p) {
-      if (p->value > value)
-        p = p->left;
-      else
-        p = p->right;
-    }
-  }
+void bst_traverse2(const struct bst* tree, void (*visitor)(int)) {
+    node_traverse2(tree->root, visitor);
 }
 
-// O(h)
-const Node* NodeSearch(const Node* node, int value) {
-  while (node) {
-    if (node->value == value)
-      return node;
-    if (node->value > value)
-      node = node->left;
-    else
-      node = node->right;
-  }
-  return node;
+void bst_traverse3(const struct bst* tree, void (*visitor)(int)) {
+    node_traverse3(tree->root, visitor);
 }
 
+void bst_insert(struct bst* tree, int value) {
+    node_insert(&tree->root, value);
+    ++tree->size;
+}
 
+int* bst_search(struct bst* tree, int value) {
+    return node_search(tree->root, value);
+}
+
+void visitor(int v) { printf("%d ", v); }
 
 int main() {
-  Node* tree;
-  NodeInit2(&tree);
+    struct bst tree;
+    bst_init(&tree);
 
-  NodeInsert(&tree, 10);
-  NodeInsert(&tree, 3);
-  NodeInsert(&tree, 1);
-  NodeInsert(&tree, 5);
-  NodeInsert(&tree, 0);
-  NodeInsert(&tree, 2);
-  NodeInsert(&tree, 4);
-  NodeInsert(&tree, 6);
-  NodeInsert(&tree, 17);
-  NodeInsert(&tree, 15);
-  NodeInsert(&tree, 21);
-  NodeInsert(&tree, 16);
-  NodeInsert(&tree, 18);
-  NodeInsert(&tree, 22);
+    bst_insert(&tree, 10);
+    bst_insert(&tree, 3);
+    bst_insert(&tree, 1);
+    bst_insert(&tree, 5);
+    bst_insert(&tree, 0);
+    bst_insert(&tree, 2);
+    bst_insert(&tree, 4);
+    bst_insert(&tree, 6);
+    bst_insert(&tree, 17);
+    bst_insert(&tree, 15);
+    bst_insert(&tree, 21);
+    bst_insert(&tree, 16);
+    bst_insert(&tree, 18);
+    bst_insert(&tree, 22);
 
-  NodeTraverse(tree, [](int value) { std::cout << value << " "; });
-  std::cout << std::endl;
+    bst_traverse(&tree, visitor);
+    printf("\n");
 
-  std::cout << std::endl;
-  NodeTraverse2(tree, [](int value) { std::cout << value << " "; });
-  std::cout << std::endl;
+    bst_traverse2(&tree, visitor);
+    printf("\n");
 
-  // NodeTraverse(tree, [](int value) { std::cout << value << " "; });
-  // std::cout << std::endl;
+    bst_traverse3(&tree, visitor);
+    printf("\n");
 
-  // NodeTraverse3(tree, [](int value) { std::cout << value << " "; });
-  // std::cout << std::endl;
-
-  std::cout << std::endl;
-  NodeTraverse(tree, [](int value) { std::cout << value << " "; });
-  std::cout << std::endl;
-
-  NodeFree(tree);
-  return 0;
+    bst_free(&tree);
+    return 0;
 }
